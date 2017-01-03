acade78300ddf65d758f6da03b7ed5184229a7cb
Rojait00 <Rojait00@gmail.com>
2017-01-03 11:39:12 +0100

    Initial Commit

---
+++ RF-Sniffer-Desktop/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace List_RCMDS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 49);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(260, 322);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(163, 23);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(95, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(122, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "COM:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(42, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(69, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(42, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 390);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ RF-Sniffer-Desktop/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO.Ports;
+
+
+namespace List_RCMDS
+{
+    public partial class Form1 : Form
+    {
+        SerialPort port;
+        Thread thSerial;
+        Dictionary<int, int> codes = new Dictionary<int, int>();
+        bool newData = true;
+        public Form1()
+        {
+            InitializeComponent();
+
+            dataGridView1.Columns.Add("Code", "Code");
+            dataGridView1.Columns.Add("CNT", "CNT");
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                port = new SerialPort("COM" + numericUpDown1.Value, 9600);
+                port.Open();
+                thSerial = new Thread(new ThreadStart(delegate
+                {
+                    while (true)
+                    {
+                        string input = "";
+                        try
+                        {
+                            while (port.BytesToRead == 0) ;
+                            while (port.BytesToRead > 0)
+                            {
+                                input += port.ReadExisting();
+                                Thread.Sleep(50);
+                            }
+                        }
+                        catch (Exception ex)
+                        { }
+                        if (input != "")
+                        {
+                            int offset = 4;
+                            List<object> ints = Tools.Strings.encode<int>(input, ';', ref offset);
+                            foreach(int c in ints)
+                            {
+                                if (codes.ContainsKey(c))
+                                    codes[c]++;
+                                else
+                                    codes.Add(c, 1);
+                            }
+                            newData = true;
+
+                        }
+
+                    }
+                }));
+                thSerial.IsBackground = true;
+                thSerial.Start();
+
+            }
+            catch
+            { }
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(port!=null)
+            port.Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (newData)
+            {
+                newData = false;
+                while (dataGridView1.Rows[0].IsNewRow == false)
+                    dataGridView1.Rows.RemoveAt(0);
+
+                for (int i = 0; i < codes.Count; i++)
+                {
+                    dataGridView1.Rows.Add(codes.Keys.ElementAt(i), codes.Values.ElementAt(i));
+                }
+                
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+    }
+}
